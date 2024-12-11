ef3b4ede5cd89d6465eef580e697d7de69d4d4f5
Carl Ulberg <ulbergc@uw.edu>
2024-12-11 03:16:06 +0000

    Remove deprecated activeMQ code

--- include/gfast_activeMQ.h
+++ include/gfast_activeMQ.h
@@ -25,31 +25,31 @@ int activeMQ_readIni(const char *propfilename,
                      struct GFAST_activeMQ_struct *activeMQ_props);
 
 /* Initialize activeMQ parameters */
-void *activeMQ_consumer_initialize(const char AMQuser[],
-                                   const char AMQpassword[],
-                                   const char AMQurl[],
-                                   const char AMQdestination[],
-                                   const int msReconnect,
-                                   const int maxAttempts,
-                                   const bool useTopic,
-                                   const bool clientAck,
-                                   const bool luseListener,
-                                   const unsigned int maxMessages,
-                                   const int verbose,
-                                   int *ierr);
+// void *activeMQ_consumer_initialize(const char AMQuser[],
+//                                    const char AMQpassword[],
+//                                    const char AMQurl[],
+//                                    const char AMQdestination[],
+//                                    const int msReconnect,
+//                                    const int maxAttempts,
+//                                    const bool useTopic,
+//                                    const bool clientAck,
+//                                    const bool luseListener,
+//                                    const unsigned int maxMessages,
+//                                    const int verbose,
+//                                    int *ierr);
 /* Get a message from activeMQ */
-char *activeMQ_consumer_getMessage(void *consumer,
-                                   const int ms_wait, int *ierr);
+// char *activeMQ_consumer_getMessage(void *consumer,
+//                                    const int ms_wait, int *ierr);
 /* Get a message from activeMQ listener */
-char *activeMQ_consumer_getMessageFromListener(void *consumer,
-                                               int *ierr);
+// char *activeMQ_consumer_getMessageFromListener(void *consumer,
+//                                                int *ierr);
 /* Shut down activeMQ consumer */
-void activeMQ_consumer_finalize(void *consumerIn);
+// void activeMQ_consumer_finalize(void *consumerIn);
 /* Shut down activeMQ producer */
-void activeMQ_producer_finalize(void *producerIn);
+// void activeMQ_producer_finalize(void *producerIn);
 /* Send a text message */
-int activeMQ_producer_sendMessage(void *producerIn,
-                                  const char *message);
+// int activeMQ_producer_sendMessage(void *producerIn,
+//                                   const char *message);
 /* Initialize the ActiveMQ producer */
 void *activeMQ_producer_initialize(const char AMQuser[],
                                    const char AMQpassword[],
@@ -66,16 +66,16 @@ char *activeMQ_setTcpURIRequest(const char *url,
 
 
 #ifndef __cplusplus
-#define GFAST_activeMQ_consumer_initialize(...)       \
-              activeMQ_consumer_initialize(__VA_ARGS__)
-#define GFAST_activeMQ_consumer_getMessage(...)       \
-              activeMQ_consumer_getMessage(__VA_ARGS__)
-#define GFAST_activeMQ_consumer_finalize(...)       \
-              activeMQ_consumer_finalize(__VA_ARGS__)
-#define GFAST_activeMQ_producer_finalize(...)       \
-              activeMQ_producer_finalize(__VA_ARGS__)
-#define GFAST_activeMQ_producer_sendMessage(...)       \
-              activeMQ_producer_sendMessage(__VA_ARGS__)
+// #define GFAST_activeMQ_consumer_initialize(...)       \
+//               activeMQ_consumer_initialize(__VA_ARGS__)
+// #define GFAST_activeMQ_consumer_getMessage(...)       \
+//               activeMQ_consumer_getMessage(__VA_ARGS__)
+// #define GFAST_activeMQ_consumer_finalize(...)       \
+//               activeMQ_consumer_finalize(__VA_ARGS__)
+// #define GFAST_activeMQ_producer_finalize(...)       \
+//               activeMQ_producer_finalize(__VA_ARGS__)
+// #define GFAST_activeMQ_producer_sendMessage(...)       \
+//               activeMQ_producer_sendMessage(__VA_ARGS__)
 #define GFAST_activeMQ_setTcpURIRequest(...)       \
               activeMQ_setTcpURIRequest(__VA_ARGS__)
 #endif /*ifndef __cplusplus*/
